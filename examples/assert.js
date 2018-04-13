33a55abf23e7adbea408e38e3977152d2f75e1af
Michael Borde <michael.borde@ubimail.fr>
2018-04-13 17:16:37 +0200

    examples

---
+++ examples/assert.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const { assert } = require('../build');
+
+try {
+  assert('hello').toBeANumber();
+} catch (e) {
+  console.error('Error caught:', e.message);
+}
---
+++ examples/is.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const { is } = require('../build');
+
+const isNumber = is('hello').aNumber();
+console.log(isNumber); // false
+
+const isString = is('hello').aString();
+console.log(isString); // true
